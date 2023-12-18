6259fece820fc6561abbf1fdeb6f931f9ac53318
Maximmax-os <128357147+Maximmax-os@users.noreply.github.com>
2023-12-18 21:18:03 +0300

    Add files via upload

---
+++ js/themes.js
@@ -0,0 +1,32 @@
+function wal1() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/1.jpg);";
+};
+
+function wal2() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/2.jpg);";
+};
+
+function wal3() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/3.jpg);";
+};
+
+function wal4() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/4.jpg);";
+};
+
+function wal5() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/5.jpg);";
+};
+
+function wal6() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/6.jpg);";
+};
+
+function wal7() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/7.jpg);";
+};
+
+function wal8() {
+	document.getElementsByClassName('body')[0].style = "background-image: url(img/wallpaper/8.jpg);";
+};
+
